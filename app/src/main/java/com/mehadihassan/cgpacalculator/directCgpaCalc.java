99b20c6efaac0721220b27e847043a82f1169f68
Mehadi Hassan <mail@mehadihassan.com>
2018-09-10 21:25:54 +0600

    Added

---
+++ app/src/main/java/com/mehadihassan/cgpacalculator/directCgpaCalc.java
@@ -0,0 +1,13 @@
+package com.mehadihassan.cgpacalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class directCgpaCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_direct_cgpa_calc);
+    }
+}
